ff3704d4e1ef44c0bee38252be5da9775d783038
Aaron Corney <30588784+Aycorn@users.noreply.github.com>
2022-07-21 12:00:50 +1200

    Update dotnet installer from 6.0.1 to 6.0.7
    Updated $dotNet6 and $dotnetLOC from 6.0.1 installer to 6.0.7 installer.

--- samples/dd-portalinstall.ps1
+++ samples/dd-portalinstall.ps1
@@ -1,13 +1,13 @@
 $urlWebView = "https://go.microsoft.com/fwlink/?linkid=2124701"
 $clientDesktop = Read-Host -Prompt 'Input your target location to download Client_desktop_app.msi'
-$dotNet6 = "https://download.visualstudio.microsoft.com/download/pr/bf058765-6f71-4971-aee1-15229d8bfb3e/c3366e6b74bec066487cd643f915274d/windowsdesktop-runtime-6.0.1-win-x64.exe"
+$dotNet6 = "https://download.visualstudio.microsoft.com/download/pr/dc0e0e83-0115-4518-8b6a-590ed594f38a/65b63e41f6a80decb37fa3c5af79a53d/windowsdesktop-runtime-6.0.7-win-x64.exe"
 $favicon = Read-Host -Prompt 'Input your target location to download favicon.png'
 $logo = Read-Host -Prompt 'Input your target location to download logo.png'
 $appsettings = Read-Host -Prompt 'Input your target location to download appsettings.json'
 
 $output = "$PSScriptRoot\webview2.exe"
 $output2 = "$PSScriptRoot\client_desktop_app.msi"
-$dotnetLOC = "$PSScriptRoot\windowsdesktop-runtime-6.0.1-win-x64.exe"
+$dotnetLOC = "$PSScriptRoot\windowsdesktop-runtime-6.0.7-win-x64.exe"
 $faviconLOC = "$PSScriptRoot\favicon.png"
 $logoLOC = "$PSScriptRoot\logo.png"
 $appsettingsLOC = "$PSScriptRoot\appsettings.json"
